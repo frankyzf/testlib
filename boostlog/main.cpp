c427b17baae5c80ab366bf96c45e27cfdd36a461
frankyzf <frankyzf@gmail.com>
2015-12-13 09:52:07 +0800

    file rotate

--- boostlog/main.cpp
+++ boostlog/main.cpp
@@ -18,13 +18,13 @@ namespace attrs = boost::log::attributes;
 namespace keywords = boost::log::keywords;
 
 void init() {
-//    logging::add_file_log(
-//            keywords::file_name = "sample_%N.log",
-//            keywords::rotation_size = 10 * 1024 * 1024,
-//            keywords::time_based_rotation = sinks::file::rotation_at_time_point(0, 0, 0),
-//            keywords::format = "[%TimeStamp%]: %Message%"
-//    );
-    logging::add_file_log("sample.log");
+    logging::add_file_log(
+            keywords::file_name = "sample_%N.log",
+            keywords::rotation_size = 10 * 1024 * 1024,
+            keywords::time_based_rotation = sinks::file::rotation_at_time_point(0, 0, 0),
+            keywords::format = "[%TimeStamp%]: %Message%"
+    );
+    //logging::add_file_log("sample.log");
     logging::core::get()->set_filter(logging::trivial::severity >= logging::trivial::info);
 }
 
